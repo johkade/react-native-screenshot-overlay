f31a895a4d0dea835caae861711249d14cf31f9f
johkade <johkade.dev@gmail.com>
2022-06-15 22:12:10 +0200

    -fix: exports

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,27 @@
-export { default } from './uiOverlay';
+import React from 'react';
+import {
+  Dimensions,
+  Image,
+  ImageSourcePropType,
+  StyleSheet,
+  View,
+} from 'react-native';
+
+const defaultSrc = require('./overlay.jpeg');
+const { height, width } = Dimensions.get('screen');
+
+type UIOverlayProps = {
+  src: ImageSourcePropType;
+  opacity?: number;
+};
+
+export const UIOverlay = ({
+  src = defaultSrc,
+  opacity = 0.25,
+}: UIOverlayProps) => {
+  return __DEV__ ? (
+    <View style={[StyleSheet.absoluteFillObject, { opacity }]}>
+      <Image source={src} style={{ height, width }} resizeMode={'stretch'} />
+    </View>
+  ) : null;
+};
